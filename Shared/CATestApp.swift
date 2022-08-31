331785da866655fb6ef75403902b5d651fb98d29
hdm007 <harry.mardirossian13@gmail.com>
2022-08-31 03:25:16 -0400

    first commit

---
+++ Shared/CATestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CATestApp.swift
+//  Shared
+//
+//  Created by Harry Mardirossian on 8/23/22.
+//
+
+import SwiftUI
+
+@main
+struct CATestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shared/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  CATest
+//
+//  Created by Harry Mardirossian on 8/31/22.
+//
+
+import Foundation
+
+struct Question {
+    let questionText: String
+    let possibleAnswers: [String]
+    let correctAnswerIndex: Int
+}
